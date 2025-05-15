59d9f2c640a472366f6ed7be47fbeecd489ca057
jamiuq <jamiuq@farmingdale.edu>
2025-05-15 19:56:18 -0400

    Create Grocery.java

---
+++ Grocery.java
@@ -0,0 +1,20 @@
+public class Grocery
+{
+  private boolean isGeneticallyModified;
+
+  public Grocery(String itemName, double price, boolean isGeneticallyModified)
+  {
+    this.isGeneticallyModified = isGeneticallyModified;
+    super(itemName, price);
+  }
+
+  public boolean getIsGeneticallyModified() {return isGeneticallyModified;}
+  public void setIsGeneticallyModified(boolean isGeneticallyModified) {this.isGeneticallyModified = isGeneticallyModified;}
+
+  @Override
+  public String toString()
+  {
+    return super.toString() + ", Genetically Modified: " + (isGeneticallyModified ? "Yes" : "No");
+  }
+}
+    
