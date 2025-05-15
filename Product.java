8491b6471f51fa061e2ad895b7af31b300718ec5
jamiuq <jamiuq@farmingdale.edu>
2025-05-15 19:45:33 -0400

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,17 @@
+public class Product
+{
+  private String itemName;
+  private double price;
+  public Product(String itemName, double price) {this.itemName = itemName; this.price = price;}
+
+  public String getItemName() {return itemName;}
+  public void setItemName(String itemName) {this.itemName = itemName;}
+
+  public double getPrice() {return price;}
+  public void setPrice(double price) {this.price = price;}
+
+  public String toString()
+  {
+    return "Name: " + itemName + ", Price: $" + String.format("%.2f", price);
+  }
+}
