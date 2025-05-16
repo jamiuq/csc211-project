960b7fec849764223b33a6b80272a090060fb3e3
jamiuq <jamiuq@farmingdale.edu>
2025-05-15 20:28:32 -0400

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -2,49 +2,59 @@
 class Customer extends User {
     private ArrayList<Product> cart;
 
-    public Customer(String username, String password) {
+    public Customer(String username, String password) 
+    {
       super(username, password);
       this.cart = new ArrayList<>();
     }
 
-    public List<Product> getCart() {
+    public List<Product> getCart() 
+    {
       return cart;
     }
 
-    public void addToCart(Product product) {
+    public void addToCart(Product product) 
+    {
       cart.add(product);
     }
 
-    public void removeFromCart(Product product) {
+    public void removeFromCart(Product product) 
+    {
       cart.remove(product);
     }
 
     public void removeFromCart(int index)
     {
       cart.remove(index);
-
+    }
+    
     // Lambda to calculate the total price of items in the cart
-    public double calculateCartTotal(numProcessor totalCalculator) {
+    public double calculateCartTotal(numProcessor totalCalculator) 
+    {
       return totalCalculator.apply(this.cart);
     }
 
     // Lambda to apply a discount to the cart total
-    public double applyDiscount(double initialTotal, numProcessor discountApplicator) {
+    public double applyDiscount(double initialTotal, numProcessor discountApplicator) 
+    {
       return discountApplicator.apply(initialTotal);
     }
 
     // Lambda to calculate tax on the cart total
-    public double calculateTax(double subtotal, numProcessor taxCalculator) {
+    public double calculateTax(double subtotal, numProcessor taxCalculator) 
+    {
       return taxCalculator.apply(subtotal);
     }
 
     // Lambda to calculate shipping fees
-    public double calculateShipping(numProcessor shippingCalculator) {
+    public double calculateShipping(numProcessor shippingCalculator) 
+    {
       return shippingCalculator.apply(this.cart);
     }
 
     @Override
-    public String toString() {
+    public String toString() 
+    {
         return super.toString() + ", Number of Cart Items: " + cart.size();
     }
 }
